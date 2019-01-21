1f0a9b7d8c4bafeb0b202254a1f60921ce0cb67b
Glitch (requestheaderparser-martin) <none>
2019-01-21 10:09:06 +0000

    🍚🐵 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api", function (req, res) {
+  var ipaddress = req.ip;
+  var language = req.acceptsLanguages();
+  var software = req.get("User-Agent").split(" ");
+  res.json({"ipaddress": ipaddress, "language": language[0], "software": software[1].slice(1)+" "+software[2]+" "+software[3].slice(0,-1)});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
